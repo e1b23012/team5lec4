06a632439948cc8eee0c095e491d7b646dad8853
7wataaa <65275342+7wataaa@users.noreply.github.com>
2025-10-21 16:29:56 +0900

    入室したことをDBに記録(Insert)する処理を実装

--- src/main/java/oit/is/team5/lec4/team5lec4/controller/Sample41Controller.java
+++ src/main/java/oit/is/team5/lec4/team5lec4/controller/Sample41Controller.java
@@ -1,11 +1,16 @@
 package oit.is.team5.lec4.team5lec4.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import oit.is.team5.lec4.team5lec4.model.Chamber;
 import oit.is.team5.lec4.team5lec4.model.ChamberMapper;
 
 @Controller
@@ -28,6 +33,21 @@ public class Sample41Controller {
 
   @GetMapping("/sample4/step3")
   public String sample43() {
+    return "sample43.html";
+  }
+
+  @PostMapping("/sample4/step3")
+  public String sample43post(@RequestParam String chamberName, ModelMap model, Principal prin) {
+    String username = prin.getName();
+
+    Chamber chamber = new Chamber();
+    chamber.setUserName(username);
+    chamber.setChamberName(chamberName);
+
+    chamberMapper.insertChamber(chamber);
+
+    model.addAttribute("chamber", chamber);
+
     return "sample43.html";
   }
 }
