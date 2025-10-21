67ad8a2e59c37a45fcc2abb1f1c972e76f3e181d
e1b23012 <e1b23012@oit.ac.jp>
2025-10-21 14:01:44 +0900

    first commit

---
+++ src/main/java/oit/is/team5/lec4/team5lec4/Team5lec4Application.java
@@ -0,0 +1,13 @@
+package oit.is.team5.lec4.team5lec4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5lec4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team5lec4Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team5/lec4/team5lec4/Team5lec4ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team5.lec4.team5lec4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team5lec4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
